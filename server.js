06584694e2a3f0cda723f87df463f1da91b8b7cb
Varun Sharma <varunited@gmail.com>
2016-01-03 16:48:24 +0530

    Updates server.js

--- server.js
+++ server.js
@@ -1,83 +1,139 @@
 var net = require('net');
 var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
 
-var request = {};
-var requestHeader = {}; //THROWS ERROR WHEN INITIALIZED INSIDE THE FUNCTION
+
+var postDump = {};
 var METHOD = {
     GET: get_handler,
     POST: post_handler
 }
-//-------------PARSING----------------------------------------------------------
-function parseProtocol(prot) {
-    requestHeader['method'] = prot[0];
-    requestHeader['path'] = prot[1];
-    requestHeader['version'] = prot[2];
+
+var CONTENT_TYPE = {
+    html: 'text/html'
 }
 
-function parseHeader(arr) {
-    arr.pop();
-    arr.pop();
+var staticFileSystem = {
+    '/': '/public/index.html'
+}
+//-------------PARSING----------------------------------------------------------
+function parseProtocol(request, prot) {
+    request['header']['method'] = prot[0];
+    request['header']['path'] = prot[1];
+    request['header']['version'] = prot[2];
+}
 
-    parseProtocol(arr[0].split(' '));
+function parseHeader(request, arr) {
+    //arr.pop();
+    //arr.pop();
+    //var requestHeader = {}; //THROWS ERROR WHEN INITIALIZED INSIDE THIS FUNCTION
+    parseProtocol(request, arr[0].split(' '));
     arr.shift();
     arr.forEach(function(data) {
         var elem = data.split(': ');
-        requestHeader[elem[0]] = elem[1];
+        request['header'][elem[0]] = elem[1];
     });
-    request['header'] = requestHeader;
-
+    //request['header'] = requestHeader;
 }
 
-/*function parseBody(arr) {
+function parseBody(request, queryString) {
+        request['body'] = queryString;
+} //FOR POST REQUEST
 
-}*/ //FOR POST REQUEST
+function responseStringify(response) {
+    var responseString = response['status'] + '\r\n';
+    for (key in response) {
+        if (response.hasOwnProperty(key) && key != 'content' && key != 'status') {
+            responseString += key + ': ' + response[key] + '\r\n';
+        }
 
-//-------------HANDLERS---------------------------------------------------------
+    }
+    responseString += '\r\n';
+    if ('content' in response) responseString += response['content'];
+    //--console.log(response);
+    //console.log(responseString);
+    return responseString;
+}
 
 function methodHandler(request,response) {
-    var handler = METHOD[request['header']['method']];
-    handler(request,response);
+    METHOD[request['header']['method']](request,response);
 }
 
 function get_handler(request, response) {
+    console.log(request);
     staticFileHandler(request,response);
 }
-function post_handler() {
 
+function post_handler(request, response) {
+    request['content'] = qs.parse(request['body']);
+    //console.log(request['content']);
+    console.log(request);
+    //request["socket"].uncork();
+    //request["socket"].end();
+    //ok200Handler(request, response);
+    staticFileHandler(request, response);
 }
 
 function staticFileHandler(request, response) {
     var filePath = false;
     if (request['header']['path'] == '/') filePath = './public/index.html';
-    var stats = fs.stat(filePath);
-    //console.log(stats);
-    var fileSize = stats['size'];
-    console.log(fileSize);
+    if (request['header']['path'] == '/form.html') filePath = './public/form.html';
+    if (request['header']['method'] == 'POST') filePath = './temp/test.html';
+
+    fs.readFile(filePath, function(err, data) {
+        if (!err) {
+            response['content'] = data.toString();
+            //--response['content'] = '<html><body><h1>Foo Bar</h1></body></html>';
+            var contentType = filePath.split('.').pop();
+            response['Content-type'] = CONTENT_TYPE[contentType];
+            ok200Handler(request, response);
+        }
+    });
 }
 
-function requestHandler(requestString) {
+function ok200Handler(request, response) {
+    response['status'] = 'HTTP/1.1 200 OK';
+    if (response['content'])  response['Content-Length'] = (response['content'].length).toString();
+    responseHandler(request, response);
+}
+
+function responseHandler(request, response) {
+    response['Date'] = new Date().toUTCString();
+    response['Connection'] = 'close';
+    response['Server'] = 'NodeServer';
+    var responseString = responseStringify(response);
+    request["socket"].write(responseString, function(err) {
+            request["socket"].end();
+    });
+    //if (response['status'] != "POST") request["socket"].end();
+}
+
+function requestHandler(request, requestString) {
     var response = {};
     var requestParts = requestString.split('\r\n\r\n');
     //var requestHeader = {};
     //var requestBody = {}; //FOR POST REQUEST
-
-    parseHeader(requestParts[0].split('\r\n'));
+    console.log("HEAD: " + requestParts[0]);
+    parseHeader(request, requestParts[0].split('\r\n'));
+    console.log("BODY: " + requestParts[1]);
+    if (requestParts[1].length) {
+        //console.log(requestParts[1]);
+        parseBody(request, requestParts[1]);
+        if (!request['header']['Content-Length']) request['header']['Content-Length'] = request['body'].length;
+    }
     //parseBody(requestParts[1].split('\n')); //FOR POST REQUEST
-    console.log(request);
+    //console.log(request);
     methodHandler(request,response);
 }
-
+//------------------------------------------------------------------------------
 net.createServer(function(socket) {
+    var request = {};
     request["socket"] = socket;
+    request['header'] = {};
+    request['body'] = {};
     socket.on('data', function(data) {
-        requestHandler(data.toString());
-
-        var result = "HTTP/1.1 200 OK\r\n\r\n<html><body><h1>Foo Bar</h1></body></html>";
-        socket.write(result);
-        socket.end();
+        //console.log('RAW-REQUEST: ' + data.toString());
+        requestHandler(request, data.toString());
     });
-
-    /*socket.on('end', function() {
-        console.log("Disconnected");
-    });*/
 }).listen(8080);
